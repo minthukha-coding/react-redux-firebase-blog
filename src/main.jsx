ce3a92162ef5928df449505468266f742a6fed4b
minthukha-coding <minthukha1239788@gmail.com>
2024-01-02 16:32:04 +0630

    Complete Reducer Setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { createStore } from "redux";
+import rootReducer from "./store/reducres/rootReducer.jsx";
+import { Provider } from "react-redux";
 
+const store = createStore(rootReducer);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+
+  <Provider store={store}>
     <Router>
-    <App />
+      <App />
     </Router>
-  </React.StrictMode>,
-)
+  </Provider>
+  // </React.StrictMode>
+);
---
+++ src/store/reducres/rootReducer.jsx
@@ -0,0 +1,11 @@
+import authReducer from "./authReducer";
+import productReducer from "./productReducer";
+import { combineReducers  } from "redux";
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    project: productReducer,
+
+})
+
+export default rootReducer;
